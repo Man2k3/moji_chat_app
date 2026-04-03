fc9b399e8392a8f0b014060a1a73aba2a4553cfc
Man <anmanproone94@gmail.com>
2026-04-03 17:14:09 +0700

    update api signin

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,11 @@
 import bcrypt from "bcrypt";
 import User from "../models/User.js";
+import Session from "../models/Session.js";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+
+const ACCESS_TOKEN_TTL = "30m"; // thường dưới 15 phút để tăng cường bảo mật
+const REFRESH_TOKEN_TTL = 14 * 24 * 60 * 60 * 1000; // 14 days in seconds
 
 export const signUp = async (req, res) => {
   try {
@@ -31,5 +37,59 @@ export const signUp = async (req, res) => {
   } catch (error) {
     console.error("Error during sign up:", error);
     return res.status(500).json({ message: "Error occurred while signing up" });
+  }
+};
+
+export const signIn = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ message: "Username or password are required" });
+    }
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    const passwordCorrect = await bcrypt.compare(password, user.hashedPassword);
+
+    if (!passwordCorrect) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: ACCESS_TOKEN_TTL,
+      },
+    );
+
+    const refreshToken = crypto.randomBytes(64).toString("hex");
+    await Session.create({
+      userId: user._id,
+      refreshToken,
+      expiresAt: new Date(Date.now() + REFRESH_TOKEN_TTL),
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none", //backend và frontend deploy riêng
+      maxAge: REFRESH_TOKEN_TTL,
+    });
+
+    return res
+      .status(200)
+      .json({
+        message: `User ${user.displayName} logged in successfully`,
+        accessToken,
+      });
+  } catch (error) {
+    console.error("Error during sign in:", error);
+    return res.status(500).json({ message: "Error occurred while signing in" });
   }
 };
